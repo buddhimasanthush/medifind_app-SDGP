edc7ea6641a4f76701af3362e2910e53445bdd7d
nisal20232064 <nisal.20232064@iit.ac.lk>
2026-03-22 00:56:17 +0530

    add doctor model

---
+++ models/Doctor.js
@@ -0,0 +1,12 @@
+// models/Doctor.js
+module.exports = (sequelize, DataTypes) => {
+  const Doctor = sequelize.define('Doctor', {
+    id: { type: DataTypes.UUID, defaultValue: DataTypes.UUIDV4, primaryKey: true },
+    fullName: { type: DataTypes.STRING, allowNull: false },
+    email: { type: DataTypes.STRING, allowNull: false, unique: true },
+    licenseNumber: { type: DataTypes.STRING, allowNull: false, unique: true },
+    phone: { type: DataTypes.STRING, allowNull: true },
+  });
+
+  return Doctor;
+};
