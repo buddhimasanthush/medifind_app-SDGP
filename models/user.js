c5df6a69c392dfd3376956cd52ca89d20fe47144
nisal20232064 <nisal.20232064@iit.ac.lk>
2026-03-22 00:54:22 +0530

    extend user model with role and phone fields

--- models/user.js
+++ models/user.js
@@ -9,6 +9,11 @@ module.exports = (sequelize, DataTypes) => {
     fullName: { type: DataTypes.STRING, allowNull: false },
     email: { type: DataTypes.STRING, allowNull: false, unique: true },
     password: { type: DataTypes.STRING, allowNull: false },
+    role: {
+      type: DataTypes.ENUM('Patient', 'Doctor', 'Pharmacy', 'Admin', 'DeliveryPartner'),
+      allowNull: false
+    },
+    phone: { type: DataTypes.STRING, allowNull: true },
   });
 
   return User;
