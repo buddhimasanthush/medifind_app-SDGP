761b32b1eae8e9dcbf8174d5d398fe1e9b234014
buddhimasanthush <buddhimasanthush@gmail.com>
2026-03-23 10:28:21 +0530

    init backend project with FastAPI and uvicorn

---
+++ backend/main.py
@@ -0,0 +1,8 @@
+from fastapi import FastAPI
+import uvicorn
+import os
+
+app = FastAPI(title="MediFind Backend API")
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
